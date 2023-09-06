526dbfb714e447ed27a64898562938c8c1e8d3ff
rezvan-asadi <rez1.programmer@gmail.com>
2023-09-06 22:04:50 +0330

    added active link for navbar

--- js/app.js
+++ js/app.js
@@ -5,23 +5,12 @@ toggler.addEventListener("click", (e) => {
   navbar.classList.toggle("nav__expanded");
 });
 
-const tabs = document.querySelectorAll(".tab");
-const tabContents = document.querySelectorAll(".tab-content");
-tabs.forEach((tab) => {
-  tab.addEventListener("click", (e) => {
-    const targetTabContent = document.querySelector(
-      `#${tab.dataset.tabTarget}`
-    );
+// ------------------ active tab ----------------------
 
-    // REMOVE PREVIOUS ACTIVED CLASSES
-    tabs.forEach((tab) => tab.classList.remove("active"));
-    tabContents.forEach((tabContent) => tabContent.classList.remove("active"));
-
-    // ADD NEW ACTIVE CLASSES
-    tab.classList.add("active");
-    targetTabContent.classList.add("active");
-  });
+const activePage = window.location.pathname;
+const navItems = document.querySelectorAll("nav a").
+forEach(link => {
+  if (link.href.includes(`${activePage}`)) {
+    link.classList.add("active");
+  }
 });
-// --------------------- links --------------------
-
-
